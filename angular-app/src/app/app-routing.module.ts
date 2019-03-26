5c2f200329a8044b3fc603362d8bf1ae9a215ede
siddharth panda <siddharth@ibm.com>
2019-03-26 13:43:20 -0500

    Added angular module

---
+++ angular-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'playlist', component: PlaylistComponent},
+  {path: 'admin', component: AdminComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
