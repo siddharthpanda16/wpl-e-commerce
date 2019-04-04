3762037d89e0e25e7482a3db6f2baf1f5f372931
Pat Dayton <daytonpe@Pats-MacBook-Pro.local>
2019-04-04 00:25:30 -0500

    user routes hardened but no encryption

--- routes/users.js
+++ routes/users.js
@@ -35,27 +35,27 @@ function getUsersRouter() {
    * NOTE: I can easily switch this to username if you prefer*/
   router.get("/users/:id", async (req, res) => {
     console.log(`GET /users/${req.params.id} hit.`);
-    User.findById(req.params.id, function(err, user) {
-      if (err) {
-        res
-          .status(400)
-          .json({ error: "Problem finding the usr by the given parameter." });
-      } else {
-        res.status(200).json({ user });
-      }
-    });
+    try {
+      const user = await User.findById(req.params.id).catch(e => {
+        throw Error("Problem finding user by ID.");
+      });
+      res.status(200).json({ user, message: "Success" });
+    } catch (e) {
+      res.status(400).json({ error: e.message });
+    }
   });
 
   /* get all users (admin) */
   router.get("/users", async (req, res) => {
     console.log(`GET /users/${req.params.id} hit.`);
-    User.find(function(err, users) {
-      if (err) {
-        res.status(400).json({ error: "problem populating all users" });
-      } else {
-        res.status(200).json({ users });
-      }
-    });
+    try {
+      const users = await User.find().catch(e => {
+        throw Error("Problem populating all users.");
+      });
+      res.status(200).json({ users });
+    } catch (e) {
+      res.status(400).json({ error: e.message });
+    }
   });
 
   /* create new user */
@@ -87,91 +87,78 @@ function getUsersRouter() {
       }
 
       /* Ensure username is not already taken. */
-      const nameTaken = await User.findOne({ username }, function(err, user) {
-        if (err) {
-          res
-            .status(400)
-            .json({ error: "Problem ensuring no duplicate username." });
-        } else {
-          return user !== null;
-        }
+      const nameTaken = await User.findOne({ username });
+
+      if (nameTaken) throw new Error("This username is already taken.");
+      const user = new User(req.body);
+
+      /* Make sure the body is saved */
+      await user.save().catch(e => {
+        throw Error("Problem saving new user.");
       });
 
-      if (nameTaken) {
-        res.status(400).json({ error: "This username is already taken." });
-      } else {
-        const user = new User(req.body);
-
-        /* Make sure the body is saved */
-        user.save(function(err) {
-          if (err) {
-            res.status(400).json({ error: "Problem saving new user." });
-          } else {
-            res.status(200).json({ id: user._id, message: "success" });
-          }
-        });
-      }
+      res.status(200).json({ id: user._id, message: "success" });
     } catch (e) {
-      console.error(e.message);
-      res.json({ error: e.message });
+      res.status(400).json({ error: e.message });
     }
   });
 
   /* update a user */
   router.put("/users/:id", async (req, res) => {
     console.log(`PUT /users/${req.params.id} hit.`);
-
-    User.findByIdAndUpdate(req.params.id, { $set: req.body }, function(
-      err,
-      user
-    ) {
-      if (err) {
-        res.status(400).json({ error: "problem finding and updating user." });
-      } else {
-        res.status(200).json({ id: user._id, success: true });
-      }
-    });
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body).catch(
+        e => {
+          throw Error("Problem finding or updating user by ID.");
+        }
+      );
+      res.status(200).json({ id: user._id, message: "Success" });
+    } catch (e) {
+      res.status(400).json({ error: e.message });
+    }
   });
 
   /* delete a user */
   router.delete("/users/:id", async (req, res) => {
-    console.log("PUT /users hit.");
-    User.findByIdAndDelete(req.params.id, { $set: req.body }, function(
-      err,
-      user
-    ) {
-      if (err) {
-        res.status(400).json({ error: "Problem finding and deleting user." });
-      } else {
-        res.status(200).json({ success: true });
-      }
-    });
+    console.log("DELETE /users hit.");
+    try {
+      await User.findByIdAndDelete(req.params.id).catch(e => {
+        throw Error("Problem finding or deleting user by ID.");
+      });
+      res.status(200).json({ message: "Success" });
+    } catch (e) {
+      res.status(400).json({ error: e.message });
+    }
   });
 
   /* Add a movie id to a users cart */
   router.post("/users/:userId/movie/:movieId", async (req, res) => {
     console.log("POST /users/:userId/movie/:movieId hit.");
     try {
-      /* Locate the user via their id */
-      User.findById(req.params.userId, function(err, user) {
-        if (err) throw new Error("Error finding user.");
-
-        /* ensure it's actually a movie */
-        Movie.findById(req.params.movieId, function(err, movies) {
-          if (err) throw new Error("Movie doesn't exist.");
-        });
-
-        /* Add the movie to the users cart */
-        user.cart.push(req.params.movieId);
-
-        /* save the new information */
-        user.save(function(err) {
-          if (err) throw new Error("Error adding movie to cart.");
-          res.status(200).json({ message: "Success" });
-        });
+      /* Ensure the user exists */
+      const user = await User.findById(req.params.userId).catch(e => {
+        throw Error("Problem finding user by ID.");
       });
+
+      /* ensure it's actually a movie */
+      await Movie.findById(req.params.movieId).catch(e => {
+        throw Error("Movie doesn't exist");
+      });
+
+      /* ensure movie isn't already in the cart. */
+      if (user.cart.includes(req.params.movieId)) {
+        throw Error("Movie already in cart.");
+      }
+
+      /* Add the movie to the users cart */
+      user.cart.push(req.params.movieId);
+
+      /* save the new information */
+      await user.save();
+
+      res.status(200).json({ cart: user.cart, message: "Success" });
     } catch (e) {
-      console.error(e.message);
+      res.status(400).json({ error: e.message });
     }
   });
 
