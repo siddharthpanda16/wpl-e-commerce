f89fc001546955e8e0fb48fc14038721a8c60bbf
pushpita panigrahi <pushpita.panigrahi@ibm.com>
2019-04-12 20:31:55 -0500

    Recommended movies page done

--- angular-app/src/app/recommended/recommended.component.ts
+++ angular-app/src/app/recommended/recommended.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
-
+import { UserService } from '../services/userServices';
+import { MovieService } from '../services/movieServices';
+import { Movie } from '../models/movie';
+import { User } from '../models/user';
+import { error } from 'util';
 
 @Component({
   selector: 'app-recommended',
@@ -10,7 +14,13 @@ import { DataService } from '../data.service';
 })
 export class RecommendedComponent implements OnInit {
 
-  constructor(private sharedData:DataService,private router:Router) { }
+  
+  movies : Movie[] = [];
+  cart : Movie[] = [];
+  user : User = new User();
+
+  constructor(private sharedData:DataService, private movieService:MovieService, 
+    private userService:UserService, private router: Router ) { }
 
   ngOnInit() {
     this.sharedData.currentUser.subscribe(user=> {
@@ -20,9 +30,48 @@ export class RecommendedComponent implements OnInit {
       }
       else{
         console.log("i am in router");
-          
+        this.setUser();
+        this.getRecommended();
         }
       });
+  }
+
+
+  public getRecommended(): void {
+    this.movieService.getRecommended( this.cart ).subscribe( movies => {
+      movies.forEach( movie => {
+        this.movies.push( movie );
+      });
+    });
+  }
+
+  public setUser(){
+    this.sharedData.currentUser.subscribe( user => {
+      this.user = user;
+      this.user.cart.forEach( movieId => {
+        this.movieService.getMovieById( movieId ).subscribe( movie => {
+          this.cart.push( movie );
+        });
+      });
+    });
+  }
+
+  public addToPlayList(movie:Movie){
+    console.log( { from : "AddPlaylistTopRated" , user : this.user , movie});
+    this.userService.addToPlaylist(this.user , movie._id);
+  }
+
+  public checkMovieInCart(movie:Movie){
+    this.user.cart.includes(movie._id);
+  }
+
+  public movieDetails(movie:Movie){
+    this.router.navigateByUrl('/movie/'+movie._id);
+  }
+
+  public canPlay(movie:Movie){
+    console.log( { from : "canPlayTopRated", user : this.user , movie});
+    movie.level <= this.user.level;
   }
 
 }
